f26c54d0a944ec18fe2d792b7d45ab21d6cd0d5b
Aditya <adityajogdand15@gmail.com>
2025-07-16 20:49:35 +0530

    generateCArd fucntion

--- FlipCardGame/script.js
+++ FlipCardGame/script.js
@@ -13,7 +13,22 @@ const cardValues  = ['🍇','🍈','🍉','🍊','🍌','🍍','🍎','🍏'];
 const shuffledCards  = [...cardValues , ...cardValues].sort(() => Math.random() - 0.5);
 
 
-function generateCards(){
 
+
+function generateCards(){
+     cards  = shuffledCards.map((value, index) => {
+        const card  = document.createElement('div');
+        card.classList.add('card');
+        card.dataset.id  = index;
+        card.dataset.value = value;
+        
+        card.addEventListener('click',flipCard);
+        
+        grid.appendChild(card); 
+        })
 }
+
+
+
+
 
